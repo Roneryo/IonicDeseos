128b0ba368b8d4411ca30e94d9f223a2edb0a27c
Roner <roner.ortega@gmail.com>
2020-05-17 04:21:22 -0500

    first commit

---
+++ src/app/components/listas/listas.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { DeseosService } from '../../services/deseos.service';
+import { Lista } from '../../models/lista.model';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-listas',
+  templateUrl: './listas.component.html',
+  styleUrls: ['./listas.component.scss'],
+})
+export class ListasComponent  {
+  @Input() terminada = true;
+  constructor( public deseosService : DeseosService,
+               private router        : Router
+    ) { 
+
+  }
+  listaSeleccionada( lista: Lista ){
+    if( this.terminada ){
+    this.router.navigateByUrl(`/tabs/tab2/agregar/${ lista.id }`);
+    }else{
+      this.router.navigateByUrl(`/tabs/tab1/agregar/${ lista.id }`)
+    }
+  }
+  borrarLista( lista: Lista ){
+    this.deseosService.borrarLista(lista);
+  }
+}
